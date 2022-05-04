dbe6e616f58e42c99389dae989a73a67f9bae2e7
João Baptista <61848873+joao-baptistaneto@users.noreply.github.com>
2022-05-04 02:03:00 -0300

    Update api.js
    arrumando ip front

--- desafio_web/src/api.js
+++ desafio_web/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/" //alterar para a sua API
+    baseURL: "http://44.194.61.39:8080/" //alterar para a sua API
 });
 
 export default api;
